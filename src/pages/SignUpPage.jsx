82f2fcadf7b808aad40172dbec0e8c08e9712cfc
LilianaJZ <ljimenezz@unal.edu.co>
2024-12-15 21:05:59 -0500

    Se inactivó una importacion en SingUpPage.jsx

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,6 +1,6 @@
 /*import React from "react";
 /*import { Link } from "react-router-dom";
-/*/import SignUpForm from "../components/SignUpForm";
+/*import SignUpForm from "../components/SignUpForm";*/
 /*import "../components/LoginForm.css";*/
 
 /*function SignUpPage() {
