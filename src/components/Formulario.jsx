4a165d01753fbc048ec02de63307706160eefddf
LilianaJZ <ljimenezz@unal.edu.co>
2024-12-15 23:37:02 -0500

    Se agrega la clase consola para efecto en login.jsx

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -3,17 +3,16 @@ import StartStopButton from "./StarEngine";
 
 
 function Formulario() {
-    return (
-      <div className="formulario">
-       {/* <p className="parrafo">¿Aún no tienes cuenta? Regístrate hoy y accede a los mejores seguros para tu vehiculo.</p>*/}
-        <form action="" className="formLogin">
+  return (
+    <div className="formulario">
+      {/* <p className="parrafo">¿Aún no tienes cuenta? Regístrate hoy y accede a los mejores seguros para tu vehiculo.</p>*/}
+      <form action="" className="formLogin">
         {/*<label htmlFor="Usuario">Usuario</label>*/}
-          <input type="text" placeholder="Usuario" className="inputA" />
-          <input type="password" placeholder="Contraseña" className="inputA" />
-          <StartStopButton/>
-        </form>
-      </div>
-    );
-  }
-  export default Formulario;
-  
+        <input type="text" placeholder="Usuario" className="inputA" />
+        <input type="password" placeholder="Contraseña" className="inputA" />
+        <StartStopButton />
+      </form>
+    </div>
+  );
+}
+export default Formulario;
